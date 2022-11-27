a628f0e8823c945e2922eaff8cdb894a76254be5
gabriellinke <gh.linke@hotmail.com>
2022-11-27 01:43:43 -0300

    Mostra memória total, livre e utilizada

--- Dashboard.py
+++ Dashboard.py
@@ -1,8 +1,6 @@
 # Documentações úteis:
 # https://dash.plotly.com/live-updates
 # https://dash.plotly.com/external-resources
-import datetime
-
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
@@ -11,34 +9,28 @@ import threading
 import time
 import os
 
-uptime = ""
 meminfo = ""
 command = ""
-
-def update_uptime():
-    while(1):
-        global uptime
-        with open('/proc/uptime' , 'r') as live_key_file_loc:
-            live_token = live_key_file_loc.read()
-        uptime = (live_token.split(' ')[0])
-        time.sleep(1)
+memory = {}
 
 def update_memory_info():
+    global meminfo
+    global memory
     while(1):
-        global meminfo
         with open('/proc/meminfo' , 'r') as live_key_file_loc:
             live_token = live_key_file_loc.read()
         meminfo = live_token
+        memory["total"] = int(os.popen('free -m  | grep ^Mem | tr -s \' \' | cut -d \' \' -f2').read())
+        memory["free"] = int(os.popen('free -m  | grep ^Mem | tr -s \' \' | cut -d \' \' -f3').read())
+        memory["used"] = int(os.popen('free -m  | grep ^Mem | tr -s \' \' | cut -d \' \' -f4').read())
         time.sleep(1)
 
 def run_command():
+    global command
     while(1):
-        global command
         command = os.popen('ls /proc').read()
         time.sleep(1)
 
-uptimeThread = threading.Thread(target=update_uptime)
-uptimeThread.start()
 meminfoThread = threading.Thread(target=update_memory_info)
 meminfoThread.start()
 commandThread = threading.Thread(target=run_command)
@@ -57,21 +49,29 @@ app.layout = html.Div(
     ])
 )
 
+def get_memory_info_container():
+    return html.Div([
+        html.Div(f'Memória total: {memory["total"]:.1f} MB'),
+        html.Div(f'Memória livre: {memory["free"]:.1f} MB'),
+        html.Div(f'Memória utilizada: {memory["used"]:.1f} MB'),
+    ], className='info-container')
+
 @app.callback(Output('main-container', 'children'),
               Input('interval-component', 'n_intervals'))
-def update_metrics(n):
+def update_info(n):
     return [
+        
         html.Div([
-            html.Div('Uptime: '+uptime, className='info-container'),
-            html.Div('Uptime: '+uptime, className='info-container')
+            html.Div('Memória total: ', className='info-container'),
+            html.Div('Meminfo: '+meminfo, className='info-container')
         ]),
         html.Div([
             html.Div('Meminfo: '+meminfo, className='info-container'),
             html.Div('Meminfo: '+meminfo, className='info-container')
         ]),
         html.Div([
-            html.Div('Comando: '+command, className='info-container'),
-            html.Div('Comando: '+command, className='info-container')
+            get_memory_info_container(),
+            html.Div(command, className='info-container')
         ])
     ]
 
